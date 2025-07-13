900107e0253881757862cc8825d5d0e4e1ca71a4
VishDevOps828 <pinky.vsb@gmail.com>
2025-07-13 15:24:52 +0530

    Update BloxFruitMasterAuto.lua

--- BloxFruitMasterAuto.lua
+++ BloxFruitMasterAuto.lua
@@ -1,1 +1,5 @@
+-- Blox Fruits Master Auto Script
+-- Works with Delta v2.781+
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VishDevOps828/BloxFruitMobileHub/main/BloxFruitMasterAuto.lua"))()
+-- Add your actual script code here!
